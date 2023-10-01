1bcee1c199cc9096f92f03408c80cd2aa4297b5e
niten <niten@fudo.org>
2023-09-30 17:37:32 -0700

    Try this again, sigh

--- mail-server.nix
+++ mail-server.nix
@@ -209,7 +209,8 @@ in {
             "auth_bind = yes"
             "auth_bind_userdn = cn=%u,${cfg.ldap.member-ou},${cfg.ldap.base}"
             "base = ${cfg.ldap.base}"
-            # "user_filter = (&(objectClass=organizationalPerson)(cn=%n))"
+            "user_filter = (&(objectClass=organizationalPerson)(cn=%n))"
+            "pass_filter = (&(objectClass=organizationalPerson)(cn=%n))"
           ]);
         target-file = "/run/dovecot-secret/ldap.conf";
       };
