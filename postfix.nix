38b2206849128aa14af66ba04e148f4a4afacced
niten <niten@fudo.org>
2026-01-24 08:48:47 -0800

    fix spf package name

--- postfix.nix
+++ postfix.nix
@@ -645,7 +645,7 @@ in {
             command = "spawn";
             args = [
               "user=nobody"
-              "argv=${pkgs.pypolicyd-spf}/bin/policyd-spf"
+              "argv=${pkgs.spf-engine}/bin/policyd-spf"
               "${policydSpfConfig}"
             ];
           };
